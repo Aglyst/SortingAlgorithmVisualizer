09fc15a37aa615ac78f767d0accda23eeebe4ae0
Arnav Saxena <arnavsaxena818@gmail.com>
2022-07-21 21:10:13 +0530

    First Commit

---
+++ SortingAlgorithmVisualizer/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SortingAlgorithmVisualizer;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
